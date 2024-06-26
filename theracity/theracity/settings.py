008f6610518648e35bf97adf9cae37b33ec1c6ef
Madu-f0r0 <duherbert@gmail.com>
2024-06-26 18:52:40 +0100

    Deploying to Vercel

--- theracity/theracity/settings.py
+++ theracity/theracity/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = 'django-insecure-07y%^vkaih%ipxu7y3-o(i$pg3+tn#x4a5a^^18n!dami%^i9$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
